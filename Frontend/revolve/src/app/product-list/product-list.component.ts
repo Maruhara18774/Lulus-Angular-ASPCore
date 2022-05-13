517e53ae0da0f8824fb5f3b6e6b5b6bc9efb896f
NguyenMyUyen <74171928+NguyenMyUyen@users.noreply.github.com>
2022-05-13 17:28:23 +0700

    Update Componets
    Product-list componet.html
    Product-list componet.ts

--- Frontend/revolve/src/app/product-list/product-list.component.ts
+++ Frontend/revolve/src/app/product-list/product-list.component.ts
@@ -7,6 +7,10 @@ import { GetProductPagingRequest } from '../model/product/getProductPagingReques
 import { ProductImage } from '../model/ProductImage';
 import { ProductLine } from '../model/ProductLine';
 import { ProductService } from '../services/product.service';
+import { DesignerApi } from '../api/designerApi';
+import { SizeApi } from '../api/sizeApi';
+import { Designer } from '../model/Designer';
+import { Size } from '../model/Size';
 
 @Component({
   selector: 'app-product-list',
@@ -18,6 +22,8 @@ export class ProductListComponent implements OnInit {
   baseURL: String = "http://localhost:4200";
   cateID:number;
   productList: Product[] = new Array<Product>();
+  designerList: Designer[] = new Array<Designer>();
+  sizeList: Size[] = new Array<Size>();
 
   constructor(private route: ActivatedRoute, private router: Router, private productService:ProductService) {}
 
@@ -29,6 +35,8 @@ export class ProductListComponent implements OnInit {
         }
       );
     this.loadProduct();
+    this.loadDesigner();
+    this.loadSize();
   }
   async loadProduct() {
     var api = new ProductApi();
@@ -37,6 +45,26 @@ export class ProductListComponent implements OnInit {
       this.productList = this.productService.convertJSONtoProductList(result.body);
     }
     else {
+      console.log('Error: ' + result.body);
+    }
+  }
+  async loadDesigner(){
+    var api = new DesignerApi();
+    var result = await api.getAll();
+    if(result.status === 200){
+      this.designerList = this.productService.convertJSONtoDesignerList(result.body);
+    }
+    else{
+      console.log('Error: ' + result.body);
+    }
+  }
+  async loadSize(){
+    var api = new SizeApi();
+    var result = await api.getAll();
+    if(result.status === 200){
+      this.sizeList = this.productService.convertJSONtoSizeList(result.body);
+    }
+    else{
       console.log('Error: ' + result.body);
     }
   }
